87d64f82b689a19e8354949d00c65973cc0ee7d6
Donnie Alimov <mralimov89@gmail.com>
2021-08-13 22:53:36 -0400

    install and config tailwind

---
+++ find-movies/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Find Movies</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <h1>Hello NEXT JS</h1>
+    </div>
+  );
+}
