bb8332a19f5b621471a8f52c7426ae7905da1e91
Donnie Alimov <mralimov89@gmail.com>
2021-08-15 16:25:42 -0400

    SSR async movie fetching

--- find-movies/pages/index.js
+++ find-movies/pages/index.js
@@ -1,7 +1,11 @@
 import Head from 'next/head';
 import Header from '../components/Header';
+import Nav from '../components/Nav';
+import Results from '../components/Results';
+import request from '../utils/request';
 
-export default function Home() {
+export default function Home({ results }) {
+  console.log(results);
   return (
     <div>
       <Head>
@@ -14,6 +18,19 @@ export default function Home() {
       {/*Nav */}
       <Nav />
       {/*Results */}
+      <Results results={results} />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const requests = await fetch(
+    `https://api.themoviedb.org/3${
+      request[genre]?.url || request.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return { props: { results: requests.results } };
 }
